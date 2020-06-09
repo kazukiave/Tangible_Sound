3bbcd7df302b54af7a2850e5f0b0774f9e006c90
kazukiave <57382140+kazukiave@users.noreply.github.com>
2020-06-09 22:50:01 +0900

    Add files via upload

---
+++ TangibleMusic/MusicGeneretor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MusicGeneretor : MonoBehaviour
+{
+    Receiver receiver;
+
+   // [Range(0,2000)]
+    public double frequency = 440;
+   // [Range(0.00f,1)]
+    public double gain = 0.05;
+
+    private double increment;
+    private double phase;
+    private double sampling_frequency = 48000;
+
+    private void Start()
+    {
+        receiver = GetComponent<Receiver>();
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        frequency = (double)receiver.sensorVal;
+        if(frequency > 100)
+        { frequency = 0; }
+        frequency *= 10;
+
+        increment = frequency * 2 * Math.PI / sampling_frequency;
+
+        for (var i = 0; i < data.Length; i = i + channels)
+        {
+            phase = phase + increment;
+            data[i] = (float)(gain * Math.Sin(phase));
+            if (channels == 2) data[i + 1] = data[i];
+            if (phase > 2 * Math.PI) phase = 0;
+        }
+    }
+}
---
+++ TangibleMusic/Receiver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+
+public class Receiver : MonoBehaviour
+{
+
+    public string portName = "COM3";
+    public int baudRate = 9600;
+
+    private SerialPort serial;
+    private bool isRunning = false;
+
+    public int sensorVal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serial = new SerialPort(portName, baudRate);
+        serial.ReadTimeout = 3000;
+        serial.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (serial.IsOpen)
+        {
+            string str = null;
+            try
+            {
+                str = serial.ReadLine();
+                if (Int32.TryParse(str, out int val))
+                {
+                    sensorVal = val;
+                }
+            }
+            catch (System.Exception e)
+            {
+
+            }
+            Debug.Log(sensorVal.ToString());
+        }
+    }
+
+    void OnDestroy()
+    {
+        serial.Close();
+        serial.Dispose();
+    }
+}
